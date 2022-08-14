a8e24e246ed85acd0430eabbeddfe3ec34a5b549
RedIrfan <89332016+RedIrfan@users.noreply.github.com>
2022-08-14 22:20:59 +0700

    updated country side stage
    - added underground section
    - added enemy trigger
    - added breaking wood mechanic
    - added camera limit event

--- Godot/Scenes/Stage/StageMaster.gd
+++ Godot/Scenes/Stage/StageMaster.gd
@@ -1,7 +1,10 @@
 class_name StageMaster
 extends Node2D
 
+export var camera_path : NodePath
+
 var player : Node setget set_player, get_player
+var camera : Node setget set_camera, get_camera
 
 var start_position : Vector2 = Vector2.ZERO
 var current_saved_position : Vector2 = Vector2.ZERO
@@ -37,7 +40,17 @@ func set_player(nplayer):
 	player = nplayer
 
 
+func set_camera(ncamera):
+	camera = ncamera
+
+
 func get_player():
 	if player == null:
 		set_player(get_tree().get_nodes_in_group("Player")[0])
 	return player
+
+
+func get_camera():
+	if camera == null:
+		set_camera(get_node(camera_path))
+	return camera
